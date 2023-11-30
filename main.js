1f176eb4f3d7f03fbf9994c50ec98dcc3364770b
Gustavo Theotonio <gustavotheotonio46@gmail.com>
2023-11-29 22:18:23 -0300

    Create first step linkme

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var btn = document.querySelector('.buttonDarkMonde')
+
+function darkMode(){
+    var element = document.body;
+    element.classList.toggle('dark-mode')
+    btn.classList.toggle('active')
+}
+
+ 
